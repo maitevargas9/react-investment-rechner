2fb0cfdd56c7963a88c5fa4bbb0aac1b8c2873ed
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2025-02-28 17:39:31 +0100

    Table main structure

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -1,1 +1,30 @@
-export default function Results() {}
+import {
+  computeInvestmentGrowth,
+  currencyFormatter
+} from "../util/investment.js";
+
+export default function Results({ input }) {
+  const rows = [
+    "Year",
+    "Investment value",
+    "Annual interest",
+    "Total interest income",
+    "Total invested capital"
+  ];
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            {rows.map((row, index) =>
+              <th key={index}>
+                {row}
+              </th>
+            )}
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+}
